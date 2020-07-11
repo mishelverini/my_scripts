c1b682788984d8b4c2ad6aecf7137500decb0a3f
Mishel Verini <mishelverini@yahoo.com>
2020-07-11 16:20:34 +0300

    feat: implement timer.js

--- timer/timer.js
+++ timer/timer.js
@@ -1,3 +1,28 @@
-export default Timer() {
-    
+export default class Timer {
+    constructor(el, time) {
+        this.el = el;
+        this.time = time;
+        this.interval;
+    }
+
+    start() {
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    stop() {
+        clearInterval(this.interval);
+    }
+
+    tick() {
+        this.time--;
+        this.render();
+        
+        if (this.time <= 0) {
+            this.stop();
+        }
+    }
+
+    render() {
+        this.el.innerHTML = this.time;
+    }
 }
