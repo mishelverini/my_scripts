a414260740e609813e041452c78abc98e87d5def
Mishel Verini <mishelverini@yahoo.com>
2020-07-11 17:38:16 +0300

    refactor: timer.js

--- timer/timer.js
+++ timer/timer.js
@@ -1,16 +1,27 @@
+// 'use strict';
+
 export default class Timer {
     constructor(el, time) {
         this.el = el;
         this.time = time;
         this.interval;
+
+        // this.render();
+        // this.start();
+        // this.stop();
+        // this.tick();
     }
 
     start() {
-        this.interval = setInterval(this.tick, 1000);
+        this.interval = setInterval(this.tick.bind(this), 1000);
     }
 
     stop() {
         clearInterval(this.interval);
+    }
+
+    render() {
+        this.el.innerHTML = this.time;
     }
 
     tick() {
@@ -20,9 +31,5 @@ export default class Timer {
         if (this.time <= 0) {
             this.stop();
         }
-    }
-
-    render() {
-        this.el.innerHTML = this.time;
     }
 }
